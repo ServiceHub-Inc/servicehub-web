4112ef48e4c483de80e6b156a982318cf493605b
Laud Gilbert <laudaccurate@gmail.com>
2022-07-18 19:19:15 +0000

    app provider and scroll-to-top utils

---
+++ components/Application/AppFooter.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { createStyles, Container, Group, ActionIcon } from "@mantine/core";
+import { BrandTwitter, Mail, BrandInstagram } from "tabler-icons-react";
+import Logo from "./AppHeader";
+import Link from "next/link";
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 10,
+    borderTop: `1px solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  inner: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingBottom: theme.spacing.sm,
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan("xs")]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}));
+
+const socials = [
+  {
+    icon: <BrandTwitter size={22} />,
+    link: "https://twitter.com/DodooRegwels",
+    label: "Twitter",
+  },
+  {
+    icon: <BrandInstagram size={22} />,
+    link: "https://www.instagram.com/quickfix_officina/",
+    label: "Instagram",
+  },
+  {
+    icon: <Mail size={22} />,
+    link: "mailto:quickfixofficina@gmail.com",
+    label: "Mail",
+  },
+];
+
+export default function Footer() {
+  const { classes } = useStyles();
+
+  return (
+    <div className={`${classes.footer} mx-12`}>
+      <div className={classes.inner}>
+        <Logo />
+        <Group spacing={0} className={classes.links} position="right" noWrap>
+          {socials.map((item) => (
+            <ActionIcon size="lg" key={item.label}>
+              <Link href={item.link} passHref>
+                <a target="_blank" className="text-primary">
+                  {item.icon}
+                </a>
+              </Link>
+            </ActionIcon>
+          ))}
+        </Group>
+      </div>
+    </div>
+  );
+}
---
+++ components/Application/AppProvider.jsx
@@ -0,0 +1,107 @@
+import PropTypes from "prop-types";
+import React from "react";
+import Head from "next/head";
+import {
+  ColorSchemeProvider,
+  MantineProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+import Router from "next/router";
+import { NotificationsProvider } from "@mantine/notifications";
+import { ModalsProvider } from "@mantine/modals";
+import Footer from "./AppFooter";
+import { HeaderMiddle as Header } from "./AppHeader";
+// import AppHeader from "./Header";
+
+const AppProvider = ({ children }) => {
+  const router = Router.useRouter();
+
+  const error = {
+    fontSize: "12px",
+    fontFamily:
+      '-apple-system,BlinkMacSystemFont,Segoe UI,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
+  };
+
+  const [colorScheme, setColorScheme] = useLocalStorage({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
+  return (
+    <>
+      <Head>
+        <title>Quickfix Officina</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          styles={{
+            PasswordInput: { error },
+            TextInput: { error },
+            NumberInput: { error },
+            Textarea: { error },
+          }}
+          classNames={{
+            Modal: {
+              modal: "max-w-[min(95vw,700px)] w-full",
+              title: "text-center w-full",
+              header:
+                "bg-primary text-center w-full rounded-md p-2 text-white font-bold",
+              body: " border border-solid border-gray-200 rounded-md p-6",
+              close: "text-white hover:text-primary",
+            },
+          }}
+          theme={{
+            fontFamily:
+              'Roboto,-apple-system , BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+            fontSize: "10px",
+            headings: {
+              fontFamily:
+                '"Roboto SlabVariable", Roboto,-apple-system , BlinkMacSystemFont, Segoe UI, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+            },
+            colorScheme: { colorScheme },
+          }}
+        >
+          <NotificationsProvider autoClose={4000}>
+            <ModalsProvider
+              modalProps={{
+                centered: true,
+                classNames: {
+                  modal: "max-w-[min(95vw,550px)] ",
+                  body: "border-none !p-2",
+                },
+              }}
+            >
+              <div className="h-screen ">
+                <Header />
+                <div className="min-h-[calc(100vh-110px)]">{children}</div>
+                <Footer />
+              </div>
+            </ModalsProvider>
+          </NotificationsProvider>
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
+};
+
+AppProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AppProvider;
